eb2708f66b70e03f629f1d57519824f3a94837a7
Dominic Tremblay <dominictremblay24@gmail.com>
2022-08-17 17:43:52 -0400

    Update index.js - using const instead of var

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Setup basic express server
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
 
 // This is how we tell our web server where to find the files to serve
 app.use(express.static('public'));
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
